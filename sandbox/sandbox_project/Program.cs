9232cd049c68bf7795a1c4808bb5003ed85d108f
Desertelk <cadenwatts111@gmail.com>
2026-03-07 21:46:24 -0500

    Homework assignemnt W01

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 public class Program
 {
@@ -8,6 +9,33 @@ public class Program
         // with any code or ideas you have that do not directly apply to
         // one of your projects.
 
-        Console.WriteLine("Hello Sandbox World!");
+        List<int> test = new() {1,2,3,4,5,6,7,8,9};
+        foreach(int item in test)
+        {
+            Console.WriteLine(item);
+        }
+        RotateListRight(test, 3);
+        foreach(int item in test)
+        {
+            Console.WriteLine(item);
+        }       
+
+    }
+
+    public static void RotateListRight(List<int> data, int amount)
+    {
+        // TODO Problem 2 Start
+        // Remember: Using comments in your program, write down your process for solving this problem
+        // step by step before you write the code. The plan should be clear enough that it could
+        // be implemented by another person..
+
+        //I create a for loop that will iterate over the list. I will remove the last amount of items and then insert them at the beginning.
+
+        for(int j = 0; j < amount; j++)
+        {
+            int temp = data[data.Count-1];
+            data.Remove(data[data.Count - 1]);
+            data.Insert(0,temp);
+        }
     }
 }
--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -14,10 +14,16 @@ public static class Arrays
         // be implemented by another person.
 
         //first I create a new list which will become the length that is indicated by the user.
-        // Then I make a for loop that will loop over the 
-        for(int i = 0);
+        // Then I make a for loop that will loop over the list. The index will be used to add the number to the array.
+        //I will also use the index to multiply the number given to fulfill the requirement.
+        double[] multiplesArray = new double[length];
+        for(int i = 0; i < length; i++)
+        {
+            double temp = number * (i + 1);
+            multiplesArray[i] = temp;
+        }
 
-        return []; // replace this return statement with your own
+        return multiplesArray; // replace this return statement with your own
     }
 
     /// <summary>
@@ -32,6 +38,15 @@ public static class Arrays
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // be implemented by another person..
+
+        //I create a for loop that will iterate over the list. I will remove the last amount of items and then insert them at the beginning.
+
+        for(int j = 0; j < amount; j++)
+        {
+            int temp = data[data.Count-1];
+            data.Remove(data[data.Count - 1]);
+            data.Insert(0,temp);
+        }
     }
 }
